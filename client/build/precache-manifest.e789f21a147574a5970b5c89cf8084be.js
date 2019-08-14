5386901e7999181585fcaae8e148ffdbbf9f359c
stevenpearce2018 <stevenpearce2018@gmail.com>
2019-08-14 17:56:42 -0400

    gitignored my build file...

---
+++ client/build/precache-manifest.e789f21a147574a5970b5c89cf8084be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247ad168911c84b36bce8913ccb14df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32745696b329b1238142",
+    "url": "/static/css/2.81cb803e.chunk.css"
+  },
+  {
+    "revision": "038f73cd4b610485b374",
+    "url": "/static/css/main.3718c11c.chunk.css"
+  },
+  {
+    "revision": "32745696b329b1238142",
+    "url": "/static/js/2.4a2a408d.chunk.js"
+  },
+  {
+    "revision": "038f73cd4b610485b374",
+    "url": "/static/js/main.7f88ada8.chunk.js"
+  },
+  {
+    "revision": "0a9f86395f8d4702ca85",
+    "url": "/static/js/runtime~main.fb107cce.js"
+  }
+]);
